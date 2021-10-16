2ab7c9d30ccfd923a2148a894362c7d1815f65e6
shiomusubi496 <69895958+shiomusubi496@users.noreply.github.com>
2021-10-16 10:18:02 +0900

    fix presser::push

--- template.cpp
+++ template.cpp
@@ -15,14 +15,14 @@ constexpr ll INF = 1e18;
 constexpr ld EPS = 1e-8;
 constexpr ld PI = 3.1415926535897932384626;
 
-template<class T, class U> inline constexpr bool chmin (T &a, const U &b) noexcept {
+template<class T, class U> inline constexpr bool chmin(T &a, const U &b) noexcept {
     if (a > b) {
         a = b;
         return true;
     }
     return false;
 }
-template<class T, class U> inline constexpr bool chmax (T &a, const U &b) noexcept {
+template<class T, class U> inline constexpr bool chmax(T &a, const U &b) noexcept {
     if (a < b) {
         a = b;
         return true;
@@ -30,7 +30,7 @@ template<class T, class U> inline constexpr bool chmax (T &a, const U &b) noexce
     return false;
 }
 
-inline constexpr ll gcd (ll a, ll b) noexcept {
+inline constexpr ll gcd(ll a, ll b) noexcept {
     while (b) {
         const ll c = a;
         a = b;
@@ -38,11 +38,11 @@ inline constexpr ll gcd (ll a, ll b) noexcept {
     }
     return a;
 }
-inline constexpr ll lcm (ll a, ll b) noexcept {
+inline constexpr ll lcm(ll a, ll b) noexcept {
     return a / gcd(a, b) * b;
 }
 
-inline constexpr ll my_pow (ll a, ll b) noexcept {
+inline constexpr ll my_pow(ll a, ll b) noexcept {
     ll res = 1;
     while (b) {
         if (b & 1) res *= a;
@@ -51,7 +51,7 @@ inline constexpr ll my_pow (ll a, ll b) noexcept {
     }
     return res;
 }
-inline constexpr ll mod_pow (ll a, ll b, ll mod) noexcept {
+inline constexpr ll mod_pow(ll a, ll b, ll mod) noexcept {
     a %= mod;
     ll res = 1;
     while (b) {
@@ -66,21 +66,21 @@ template<class F> class rec_lambda {
   private:
     F f;
   public:
-    explicit constexpr rec_lambda (F&& f_) : f(std::forward<F>(f_)) {}
-    template<class... Args> constexpr auto operator () (Args&&... args) const {
+    explicit constexpr rec_lambda(F&& f_) : f(std::forward<F>(f_)) {}
+    template<class... Args> constexpr auto operator()(Args&&... args) const {
         return f(*this, std::forward<Args>(args)...);
     }
 };
 
-template<class T, class Arg> constexpr std::vector<T> make_vec (size_t n, Arg&& arg) {
+template<class T, class Arg> constexpr std::vector<T> make_vec(size_t n, Arg&& arg) {
     return std::vector<T>(n, arg);
 }
-template<class T, class... Args> constexpr auto make_vec (size_t n, Args&&... args) {
+template<class T, class... Args> constexpr auto make_vec(size_t n, Args&&... args) {
     return std::vector<decltype(make_vec<T>(args...))>
                (n, make_vec<T>(std::forward<Args>(args)...));
 }
 
-inline constexpr int popcnt (ull x) {
+inline constexpr int popcnt(ull x) {
 #if __cplusplus >= 202002L
     return std::popcount(x);
 #endif
@@ -92,29 +92,31 @@ inline constexpr int popcnt (ull x) {
     return (x & 0x00000000ffffffff) + ((x >> 32) & 0x00000000ffffffff);
 }
 
-template<class T> class presser : public std::vector<T>{
+template<class T> class presser : public std::vector<T> {
   private:
     using size_type = size_t;
   public:
     presser() = default;
     presser(const std::vector<T>& vec) : std::vector<T>(vec) {}
-    void push(const std::vector<T>& vec){
-        this->reserve(vec.size());
+    void push(const std::vector<T>& vec) {
+        size_type n = this->size();
+        this->resize(n + vec.size());
+        std::copy(vec.begin(), vec.end(), this->begin() + n);
     }
-    size_type build () {
+    size_type build() {
         std::sort(this->begin(), this->end());
         this->erase(std::unique(this->begin(), this->end()), this->end());
         return this->size();
     }
-    size_type get_index (const T& val) const {
+    size_type get_index(const T& val) const {
         return std::lower_bound(this->begin(), this->end(), val) - this->begin();
     }
-    std::vector<size_type> pressed (const std::vector<T>& vec) const {
+    std::vector<size_type> pressed(const std::vector<T>& vec) const {
         std::vector<size_type> res(vec.size());
         rep(i, vec.size()) res[i] = this->get_index(vec[i]);
         return res;
     }
-    void press (std::vector<T>& vec) const {
+    void press(std::vector<T>& vec) const {
         static_assert(std::is_integral<T>::value);
         rep(i, vec.size()) vec[i] = this->get_index(vec[i]);
     }
