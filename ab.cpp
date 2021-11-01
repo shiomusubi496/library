898504543c82e06d175a9a4045bc02b8dbd1bd7a
shiomusubi496 <69895958+shiomusubi496@users.noreply.github.com>
2021-11-01 19:59:45 +0900

    Delete ab.cpp

--- ab.cpp
+++
@@ -1,8 +0,0 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
-#include "../../template.cpp"
-using namespace std;
-int main() {
-    int a, b;
-    cin >> a >> b;
-    cout << a << b << endl;
-}
