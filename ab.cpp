105d4c2fe86b915aafbd38a58166349eda54bb34
shiomusubi496 <69895958+shiomusubi496@users.noreply.github.com>
2021-11-01 19:51:41 +0900

    Create ab.cpp

---
+++ ab.cpp
@@ -0,0 +1,8 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "../../template.cpp"
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a << b << endl;
+}
