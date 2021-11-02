53569b60f366278d103291b1d7ddc66bc363ec5f
shiomusubi496 <69895958+shiomusubi496@users.noreply.github.com>
2021-11-02 11:43:52 +0900

    Create ALDS1_1_B-GCD.test.cpp

---
+++ test/aoj/ALDS1_1_B-GCD.test.cpp
@@ -0,0 +1,6 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_1_B"
+#include "../../template.cpp"
+int main() {
+    int x, y; std::cin >> x >> y;
+    std::cout << gcd(x, y) << std::endl;
+}
