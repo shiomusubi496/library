931aa638fba13cd06a701bede2451ad5f82e94d5
shiomusubi496 <katsuki.ohta@gmail.com>
2021-11-04 13:57:26 +0900

    Create shortest-path restore

---
+++ graph/shortest-path/Restore.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../../template.hpp"
+#include "../Graph.hpp"
+
+template<class T> std::vector<int> Restore(const Graph<T>& G, const std::vector<T>& dist, int start = 0) {
+    int N = G.size();
+    std::vector<int> bfr(N, -2); bfr[start] = -1;
+    std::queue<int> que; que.push(start);
+    while (!que.empty()) {
+        int v = que.front(); que.pop();
+        for (const edge<T>& e : G[v]) {
+            if (bfr[e.to] == -2 && dist[e.to] == dist[v] + e.cost) {
+                bfr[e.to] = v;
+                que.push(e.to);
+            }
+        }
+    }
+    return bfr;
+}
+
+/*
+@brief Restore(経路復元)
+@docs docs/Restore.md
+*/
---
+++ test/yosupo/shortest-path.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/shortest_path"
+#include "../../template.hpp"
+#include "../../graph/Graph.hpp"
+#include "../../graph/shortest-path/Dijkstra.hpp"
+#include "../../graph/shortest-path/Restore.hpp"
+using namespace std;
+int main() {
+    int N, M, s, t; cin >> N >> M >> s >> t;
+    Graph<ll> G(N);
+    rep (i, M) {
+        int a, b, c; cin >> a >> b >> c;
+        G.add_edge(a, b, c, true);
+    }
+    vector<ll> D = Dijkstra(G, s);
+    vector<int> R = Restore(G, D, s);
+    if (R[t] == -2) {
+        puts("-1");
+        return 0;
+    }
+    vector<int> ans{t};
+    while (ans.back() != s) ans.push_back(R[ans.back()]);
+    reverse(ans.begin(), ans.end());
+    cout << D[t] << ' ' << ans.size() - 1 << endl;
+    rep (i, ans.size() - 1) cout << ans[i] << ' ' << ans[i + 1] << endl;
+}
