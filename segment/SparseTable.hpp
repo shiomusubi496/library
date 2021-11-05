d58ce59adcf18e19aec144bf2038af98d171e98c
shiomusubi496 <katsuki.ohta@gmail.com>
2021-11-05 10:37:27 +0900

    Create SparseTable

---
+++ segment/SparseTable.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../template.hpp"
+
+template<class T> class SparseTable {
+  protected:
+    using F = std::function<T(T, T)>;
+    using G = std::function<T()>;
+    int h;
+    const F& op;
+    const G& e;
+    std::vector<int> logtable;
+    std::vector<std::vector<T>> data;
+  public:
+    SparseTable(const std::vector<T>& v, const F& _op, const G& _e) : op(_op), e(_e) {
+        h = 1;
+        while ((1 << h) < (int)v.size()) ++h;
+        logtable.resize(1 << h, 0);
+        for (int i = 2; i < (1 << h); i++) logtable[i] = logtable[i >> 1] + 1;
+        data.resize(h + 1, std::vector<T>(1 << h, e()));
+        rep (i, v.size()) data[0][i] = v[i];
+        rep (i, h) {
+            rep (j, (1 << h) - (1 << i)) {
+                data[i + 1][j] = op(data[i][j], data[i][j + (1 << i)]);
+            }
+        }
+    }
+    T query(int l, int r) {
+        assert(0 <= l && l <= r && r <= (1 << h));
+        if (l == r) return e();
+        int d = logtable[r - l];
+        return op(data[d][l], data[d][r - (1 << d)]);
+    }
+};
---
+++ test/yosupo/staticrmq.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+#include "../../segment/SparseTable.hpp"
+using namespace std;
+int main() {
+    int N, Q; cin >> N >> Q;
+    vector<int> A(N);
+    for (auto&& i : A) cin >> i;
+    SparseTable<int> ST(A, [](int a, int b) -> int { return min(a, b); }, []() -> int { return INF<int>; });
+    vector<int> res;
+    rep (i, Q) {
+        int l, r; cin >> l >> r;
+        cout << ST.query(l, r) << endl;
+    }
+}
