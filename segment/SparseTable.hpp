78d5a44a42f3177606eac0954e382fc2ec329e44
shiomusubi496 <katsuki.ohta@gmail.com>
2021-11-05 14:12:13 +0900

    Fix SparseTable and docs

--- segment/SparseTable.hpp
+++ segment/SparseTable.hpp
@@ -5,19 +5,18 @@
 template<class T> class SparseTable {
   protected:
     using F = std::function<T(T, T)>;
-    using G = std::function<T()>;
     int h;
-    const F& op;
-    const G& e;
+    F op;
     std::vector<int> logtable;
     std::vector<std::vector<T>> data;
   public:
-    SparseTable(const std::vector<T>& v, const F& _op, const G& _e) : op(_op), e(_e) {
+    SparseTable() = default;
+    SparseTable(const std::vector<T>& v, const F& _op) : op(_op) {
         h = 1;
         while ((1 << h) < (int)v.size()) ++h;
         logtable.resize((1 << h) + 1, 0);
         for (int i = 2; i <= (1 << h); i++) logtable[i] = logtable[i >> 1] + 1;
-        data.resize(h + 1, std::vector<T>(1 << h, e()));
+        data.resize(h + 1, std::vector<T>(1 << h));
         rep (i, v.size()) data[0][i] = v[i];
         rep (i, h) {
             rep (j, (1 << h) - (1 << i)) {
@@ -26,9 +25,13 @@ template<class T> class SparseTable {
         }
     }
     T query(int l, int r) {
-        assert(0 <= l && l <= r && r <= (1 << h));
-        if (l == r) return e();
+        assert(0 <= l && l < r && r <= (1 << h));
         int d = logtable[r - l];
         return op(data[d][l], data[d][r - (1 << d)]);
     }
 };
+
+/**
+ * @brief SparseTable
+ * @doc docs/SparseTable.md
+ */
--- test/yosupo/staticrmq.test.cpp
+++ test/yosupo/staticrmq.test.cpp
@@ -5,7 +5,7 @@ int main() {
     int N, Q; cin >> N >> Q;
     vector<int> A(N);
     for (auto&& i : A) cin >> i;
-    SparseTable<int> ST(A, [](int a, int b) -> int { return min(a, b); }, []() -> int { return INF<int>; });
+    SparseTable<int> ST(A, [](int a, int b) -> int { return min(a, b); });
     vector<int> res;
     rep (i, Q) {
         int l, r; cin >> l >> r;
