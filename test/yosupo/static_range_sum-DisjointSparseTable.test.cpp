7d39858066d55f875eebd06c8daef882964beac4
shiomusubi496 <katsuki.ohta@gmail.com>
2021-11-07 07:36:28 +0900

    Add DisjointSparseTable test

---
+++ test/yosupo/static_range_sum-DisjointSparseTable.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
+#include "../../template.hpp"
+#include "../../segment/DisjointSparseTable.hpp"
+using namespace std;
+int main() {
+    int N, Q; cin >> N >> Q;
+    vector<ll> A(N); cin >> A;
+    DisjointSparseTable<ll> DST(A, [](ll a, ll b) { return a + b; });
+    rep (Q) {
+        int l, r; cin >> l >> r;
+        cout << DST.query(l, r) << endl;
+    }
+}
