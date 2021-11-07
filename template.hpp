7365d67c03a81d295ce135d970f86e4e05a1995d
shiomusubi496 <katsuki.ohta@gmail.com>
2021-11-07 11:07:03 +0900

    Fix CRT

--- template.hpp
+++ template.hpp
@@ -153,13 +153,16 @@ PLL ChineseRemainder(ll b1, ll m1, ll b2, ll m2) noexcept {
     const PLL p = extGCD(m1, m2);
     const ll g = p.first * m1 + p.second * m2;
     const ll l = m1 / g * m2;
-    assert((b2 - b1) % g == 0);
+    if((b2 - b1) % g != 0) return PLL{-1, -1};
     const ll x = (b2 - b1) / g * p.first % (m2 / g);
     return {(x * m1 + b1 + l) % l, l};
 }
 PLL ChineseRemainders(const std::vector<ll>& b, const std::vector<ll>& m) noexcept {
     PLL res{0, 1};
-    rep (i, b.size()) res = ChineseRemainder(res.first, res.second, b[i], m[i]);
+    rep (i, b.size()) {
+        res = ChineseRemainder(res.first, res.second, b[i], m[i]);
+        if (res.first == -1) return res;
+    }
     return res;
 }
 
