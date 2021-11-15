6eefa5db23c3f8a8ecb990609215c07bf31496e7
shiomusubi496 <katsuki.ohta@gmail.com>
2021-11-15 17:58:15 +0900

    Add dijkstra tests

---
+++ test/aoj/ALDS1/ALDS1_12_B-Dijkstra.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_12_B"
+#include "../../../template.hpp"
+#include "../../../graph/Graph.hpp"
+#include "../../../graph/shortest-path/Dijkstra.hpp"
+using namespace std;
+int main() {
+    int N; cin >> N;
+    Graph<int> G(N);
+    rep (N) {
+        int v, k; cin >> v >> k;
+        G[v].reserve(k);
+        rep (k) {
+            int u, c; cin >> u >> c;
+            G.add_edge(v, u, c, true);
+        }
+    }
+    auto v = Dijkstra(G);
+    rep (i, N) {
+        cout << i << ' ' << v[i] << endl;
+    }
+}
