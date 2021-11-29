71eafe74063a0d25be243a84be3f7d6700cc40ed
shiomusubi496 <katsuki.ohta@gmail.com>
2021-11-29 17:30:36 +0900

    Add infinity::mvalue, max, min

--- other/template.hpp
+++ other/template.hpp
@@ -44,6 +44,9 @@ template<class T> using prique = std::priority_queue<T, std::vector<T>, std::gre
 template<class T> class infinity {
   public:
     static constexpr T value = std::numeric_limits<T>::max() / 2;
+    static constexpr T mvalue = std::numeric_limits<T>::min() / 2;
+    static constexpr T max = std::numeric_limits<T>::max();
+    static constexpr T min = std::numeric_limits<T>::min();
 };
 
 #if __cplusplus >= 201402L
@@ -159,7 +162,7 @@ PLL ChineseRemainder(ll b1, ll m1, ll b2, ll m2) noexcept {
     const PLL p = extGCD(m1, m2);
     const ll g = p.first * m1 + p.second * m2;
     const ll l = m1 / g * m2;
-    if((b2 - b1) % g != 0) return PLL{-1, -1};
+    if ((b2 - b1) % g != 0) return PLL{-1, -1};
     const ll x = (b2 - b1) / g * p.first % (m2 / g);
     return {(x * m1 + b1 + l) % l, l};
 }
