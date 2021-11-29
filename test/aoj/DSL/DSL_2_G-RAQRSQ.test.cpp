00a0b9d897b91d7c4d5ab13f149d65061e6119c5
shiomusubi496 <katsuki.ohta@gmail.com>
2021-11-29 20:41:17 +0900

    Fix overflow

--- test/aoj/DSL/DSL_2_G-RAQRSQ.test.cpp
+++ test/aoj/DSL/DSL_2_G-RAQRSQ.test.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main() {
     int n, q; cin >> n >> q;
-    RangeAddQueryRangeSumQuery<int, int> seg(n);
+    RangeAddQueryRangeSumQuery<ll, ll> seg(n);
     rep (q) {
         int t; cin >> t;
         if (t == 0) {
