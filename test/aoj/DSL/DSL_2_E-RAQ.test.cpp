fbaf2f95d7993d2d56156cef8047f846f2526194
shiomusubi496 <katsuki.ohta@gmail.com>
2021-12-02 17:45:04 +0900

    Fix 0-origin

--- test/aoj/DSL/DSL_2_E-RAQ.test.cpp
+++ test/aoj/DSL/DSL_2_E-RAQ.test.cpp
@@ -9,11 +9,11 @@ int main() {
         int t; cin >> t;
         if (t == 0) {
             int l, r, x; cin >> l >> r >> x;
-            RAQ.apply(l - 1, r, x);
+            RAQ.apply(l, r + 1, x);
         }
         else {
             int k; cin >> k;
-            cout << RAQ.get(k - 1) << endl;
+            cout << RAQ.get(k) << endl;
         }
     }
 }
