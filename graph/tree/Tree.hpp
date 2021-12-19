6cde13bf79d6575ed3cedd05a9191a7123964479
shiomusubi496 <katsuki.ohta@gmail.com>
2021-12-19 17:47:53 +0900

    Create Tree

--- graph/tree/Tree.hpp
+++ graph/tree/Tree.hpp
@@ -1,44 +1,93 @@
 #pragma once
 
+#include "../../other/template.hpp"
+#include "../../other/bitop.hpp"
 #include "../Graph.hpp"
 
-template<class T> class Tree : public Graph<T> {
-  private:
-    using Base = Graph<T>;
+template<class T> class Tree {
   protected:
+    int root, n, h;
+    Graph<T> G_;
+    const Graph<T>& G;
+    std::vector<edge<T>> par;
+    std::vector<int> dep;
+    std::vector<std::vector<int>> dbl;
     void dfs_build(int v, int p) {
-        par[v] = p;
-        for (const edge<T>& e : (*this)[v]) {
-            if (e.to != p) dfs_build(e.to, v);
+        for (const edge<T>& e : G[v]) {
+            if (e.to != p) {
+                par[e.to] = edge<T>(e.to, e.from, e.cost, e.idx);
+                dep[e.to] = dep[v] + 1;
+                dfs_build(e.to, v);
+            }
+        }
+    }
+    void init() {
+        n = G.size();
+        h = bitop::ceil_log2(n);
+        par.resize(n); par[root] = edge<T>{};
+        dep.resize(n); dep[root] = 0;
+        dfs_build(root, -1);
+        dbl.assign(n, std::vector<int>(h, -1));
+        rep (i, n) dbl[i][0] = par[i].to;
+        rep (i, h - 1) {
+            rep (j, n) dbl[j][i + 1] = dbl[j][i] == -1 ? -1 : dbl[ dbl[j][i] ][i];
         }
     }
   public:
-    int root;
-    std::vector<int> par;
-    using Base::Base;
-    Tree(const Base& G) : Base(G) {}
-    Tree(Base&& G) : Base(std::move(G)) {}
-    void build(int r = 0) {
-        assert(this->edge_size() + 1 == (int)this->size());
-        assert(0 <= r && r < (int)this->size());
-        par.resize(this->size());
-        dfs_build(r, -1);
-        root = r;
+    Tree(const Graph<T>& G, int r = 0) : G(G), root(r) { init(); }
+    Tree(Graph<T>&& G, int r = 0) : G_(std::move(G)), G(G_), root(r) { init(); }
+    int depth(int v) const { return dep[v]; }
+    int parent(int v) const { return par[v].to; }
+    int kth_ancestor(int v, int k) const {
+        if (dep[v] < k) return -1;
+        rrep (i, h) {
+            if ((k >> i) & 1) v = dbl[v][i];
+        }
+        return v;
     }
-    Graph<T> child() const {
-        Graph<T> res(this->size());
-        rep (i, this->size()) {
-            if (i == root) res[i].reserve(this->size());
-            else res[i].reserve(this->size() - 1);
-            for (const edge<T>& e : (*this)[i]) {
-                if (e.to != par[i]) res.add_edge(i, e.to, e.cost, true);
+    int nxt(int s, int t) const {
+        assert(s != t);
+        if (dep[s] >= dep[t]) return parent(s);
+        int v = kth_ancestor(t, dep[t] - dep[s] - 1);
+        return parent(v) == s ? v : parent(s);
+    }
+    Edges<T> path(int s, int t) const {
+        Edges<T> pre, suf;
+        while (dep[s] > dep[t]) pre.push_back(par[s]), s = par[s].to;
+        while (dep[t] > dep[s]) suf.push_back(par[t]), t = par[t].to;
+        while (s != t) {
+            pre.push_back(par[s]), s = par[s].to;
+            suf.push_back(par[t]), t = par[t].to;
+        }
+        rrep (i, suf.size()) pre.emplace_back(suf[i].to, suf[i].from, suf[i].cost, suf[i].idx);
+        return pre;
+    }
+    int lca(int u, int v) const {
+        if (dep[u] > dep[v]) u = kth_ancestor(u, dep[u] - dep[v]);
+        if (dep[u] < dep[v]) v = kth_ancestor(v, dep[v] - dep[u]);
+        rrep (i, h) {
+            if (dbl[u][i] != dbl[v][i]) {
+                u = dbl[u][i];
+                v = dbl[v][i];
             }
         }
+        return parent(u);
+    }
+    Graph<T> rooted() const {
+        Graph<T> res(G.size());
+        rep (i, G.size()) {
+            if (i == root) res[i].reserve(G[i].size());
+            else res[i].reserve(G[i].size() - 1);
+            for (const edge<T>& e : G[i]) {
+                if (e.to != par[i].to) res[i].push_back(e);
+            }
+        }
+        res.edge_id = G.edge_size();
         return res;
     }
 };
 
 /**
- * @brief Tree-template
+ * @brief Tree(木のクエリ詰め合わせ)
  * @docs docs/Tree.md
  */
---
+++ test/aoj/GRL/GRL_5_C-LCA.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/GRL_5_C"
+#include "../../../other/template.hpp"
+#include "../../../graph/Graph.hpp"
+#include "../../../graph/tree/Tree.hpp"
+using namespace std;
+int main() {
+    int N; cin >> N;
+    Graph<int> G(N);
+    rep (i, N) {
+        int k; cin >> k;
+        rep (k) {
+            int t; cin >> t;
+            G.add_edge(i, t, true);
+        }
+    }
+    Tree<int> T(G);
+    int Q; cin >> Q;
+    rep (i, Q) {
+        int a, b; cin >> a >> b;
+        cout << T.lca(a, b) << endl;
+    }
+}
