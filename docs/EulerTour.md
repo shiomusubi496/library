097f7bbd3a174bab9026579e3aaae64f30451006
shiomusubi496 <katsuki.ohta@gmail.com>
2022-01-09 22:27:44 +0900

    Create EulerTour

---
+++ docs/EulerTour.md
@@ -0,0 +1,11 @@
+## 概要
+
+木を dfs 順に並べて列にする。 SegmentTree など列に対するデータ構造を使うことで様々なクエリを扱える。テンプレートに Static Range Minimum Query を扱える構造 StaticRMQ(デフォルトは SparseTable)をとる。
+
+- `EulerTour(Graph<T> G, int r = 0)` : 木 `G` に対して、頂点 `r` を根としてオイラーツアーを作成する。 StaticRMQ::init が $\Theta(f(N))$ で動くとき、 $\Theta(N + f(N))$ 。
+- `pair<int, int> get_idx(int v)` : 頂点 `v` に入る index と出る index のペアを返す。 $\Theta(1)$ 。
+- `int lca(int v, int u)` : 頂点 `v` と `u` の最小共通祖先(Lowest Common Ancestor)を返す。 StaticRMQ::prod が $\Theta(f(N))$ で動くとき、 $\Theta(f(N))$ 。
+- `void each_vertex_subtree(int v, void f(int, int))` : 頂点 `v` の部分木の頂点の区間に `f` を適用する。頂点に入る index に値が記録され、残りはは単位元である必要がある。計算量は `f` のそれに比例。
+- `void each_edge_subtree(int v, void f(int, int))` : 頂点 `v` の部分木の辺。
+- `void each_vertex(int u, int v, void f(int, int), void g(int, int))` : `u` - `v` パスの頂点の区間に `f` を適用する。 `f` は親->子の順に計算した値、 `g` は子->親の値を返す。頂点に入る index には元の値が、頂点から出る index にはその逆元が入っている必要がある。計算量は `f` と `g` のそれに比例。
+- `void each_edge(int u, int v, void f(int, int), void g(int, int))` : `u` - `v` パスの辺。
---
+++ graph/tree/EulerTour.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "../../other/template.hpp"
+#include "../../data-struct/segment/SparseTable.hpp"
+#include "../Graph.hpp"
+
+namespace Monoid {
+    struct PairMinForEulerTour {
+        using value_type = std::pair<int, int>;
+        static value_type op(const value_type& a, const value_type& b) {
+            return a.first < b.first ? a : b;
+        }
+        static value_type id() {
+            return {infinity<int>::value, -1};
+        }
+    };
+}
+
+template<class T, template<class> class StaticRMQ = SparseTable>
+class EulerTour {
+  protected:
+    int n, root, cnt;
+    const Graph<T>& G;
+    std::vector<int> dep;
+    std::vector<std::pair<int, int>> idx;
+    std::vector<std::pair<int, int>> rmqvec;
+    StaticRMQ<Monoid::PairMinForEulerTour> RMQ;
+    void dfs(int v, int p) {
+        idx[v].first = cnt++;
+        rmqvec.emplace_back(dep[v], v);
+        for (const edge<T>& e : G[v]) {
+            if (e.to == p) continue;
+            dep[e.to] = dep[v] + 1;
+            dfs(e.to, v);
+            rmqvec.emplace_back(dep[v], v);
+        }
+        idx[v].second = cnt++;
+    }
+    void init() {
+        n = G.size();
+        dep.resize(n); dep[root] = 0;
+        idx.resize(n); rmqvec.reserve(n << 1);
+        cnt = 0;
+        dfs(root, -1);
+        rmqvec.emplace_back(infinity<int>::value, -1);
+        RMQ.init(rmqvec);
+    }
+  public:
+    EulerTour(const Graph<T>& G, int root = 0) : G(G), root(root) { init(); }
+    const std::pair<int, int>& get_idx(int k) const& { return idx[k]; }
+    std::pair<int, int> get_idx(int k) && { return std::move(idx[k]); }
+    int lca(int u, int v) const {
+        return RMQ.prod(
+            std::min(idx[u].first, idx[v].first),
+            std::max(idx[u].second, idx[v].second) + 1
+        ).second;
+    }
+    template<class F> void each_vertex_subtree(int v, const F& f) const {
+        f(idx[v].first, idx[v].second + 1);
+    }
+    template<class F> void each_edge_subtree(int v, const F& f) const {
+        f(idx[v].first + 1, idx[v].second + 1);
+    }
+    template<class F, class G> void each_vertex(int u, int v, const F& f, const G& g) const {
+        int l = lca(u, v);
+        g(idx[l].first, idx[u].first + 1);
+        f(idx[l].first + 1, idx[v].first + 1);
+    }
+    template<class F> void each_vertex(int u, int v, const F& f) const { each_vertex(u, v, f, f); }
+    template<class F, class G> void each_edge(int u, int v, const F& f, const G& g) const {
+        int l = lca(u, v);
+        g(idx[l].first + 1, idx[u].first + 1);
+        f(idx[l].first + 1, idx[v].first + 1);
+    }
+    template<class F> void each_edge(int u, int v, const F& f) const { each_edge(u, v, f, f); }
+};
