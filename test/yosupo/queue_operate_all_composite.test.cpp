6c206d53e89f4b1b0ccd12b4ba8bdeb38adf2e77
shiomusubi496 <katsuki.ohta@gmail.com>
2022-02-15 09:30:07 +0900

    Change name

--- test/yosupo/queue_operate_all_composite.test.cpp
+++ test/yosupo/queue_operate_all_composite.test.cpp
@@ -2,7 +2,7 @@
 #include "../../other/template.hpp"
 #include "../../other/monoid2.hpp"
 #include "../../math/ModInt.hpp"
-#include "../../data-struct/structure/SlidingWindowAggregation.hpp"
+#include "../../data-struct/sequence/SlidingWindowAggregation.hpp"
 using namespace std;
 using mint = modint998244353;
 int main() {
