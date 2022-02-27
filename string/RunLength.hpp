a7c5f1d3c4080d585e1f81679f1a19403421158b
shiomusubi496 <katsuki.ohta@gmail.com>
2022-02-27 16:46:35 +0900

    Fix RunLength

--- string/RunLength.hpp
+++ string/RunLength.hpp
@@ -2,15 +2,20 @@
 
 #include "../other/template.hpp"
 
-template<class Cont> std::vector<std::pair<typename Cont::value_type, int>> RunLength(const Cont& str) {
+template<class Cont, class Comp>
+std::vector<std::pair<typename Cont::value_type, int>> RunLength(const Cont& str, const Comp& cmp) {
     std::vector<std::pair<typename Cont::value_type, int>> res;
     if (str.size() == 0) return res;
     res.emplace_back(str[0], 1);
     rep (i, 1, str.size()) {
-        if (res.back().first == str[i]) ++res.back().second;
+        if (cmp(res.back().first, str[i])) ++res.back().second;
         else res.emplace_back(str[i], 1);
     }
     return res;
+}
+
+template<class Cont> std::vector<std::pair<typename Cont::value_type, int>> RunLength(const Cont& str) {
+    return RunLength(str, std::equal_to<typename Cont::value_type>());
 }
 
 /**
