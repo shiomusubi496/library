78c980e6249f7e985c042310a06924e501b469f9
shiomusubi496 <katsuki.ohta@gmail.com>
2022-03-06 12:22:01 +0900

    Create KthRoot

---
+++ docs/KthRoot.md
@@ -0,0 +1,3 @@
+## 概要
+
+非負整数 `n` と正整数 `k` に対して、 $\lfloor \sqrt[k]{n} \rfloor$ を求める。 `std::sqrt` を使っている。
---
+++ math/KthRoot.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../other/template.hpp"
+
+ull kth_root(ull n, ull k) {
+    if (n <= 1 || k == 1) return n;
+    if (k >= 65) return 1;
+    auto is_over = [&](ull x) -> bool {
+        __uint128_t res = 1, a = x;
+        for (int b = k; b; b >>= 1, a *= a) {
+            if (b & 1) res *= a;
+        }
+        return res > n;
+    };
+    ll res = pow(n, 1.0 / k);
+    while (!is_over(res + 1)) ++res;
+    while ( is_over(res)) --res;
+    return res;
+}
+
+/**
+ * @brief KthRoot(k乗根)
+ * @docs docs/KthRoot.md
+ */
