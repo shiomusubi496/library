a8e996949a361dff9fae25d25ade07f94a23e92f
shiomusubi496 <katsuki.ohta@gmail.com>
2022-07-09 10:47:11 +0900

    Fix monoid

--- other/monoid.hpp
+++ other/monoid.hpp
@@ -14,13 +14,13 @@ template<class T> struct Sum {
 
 template<class T, T max_value = infinity<T>::max> struct Min {
     using value_type = T;
-    static constexpr T op(const T& a, const T& b) { return a > b ? b : a; }
+    static constexpr T op(const T& a, const T& b) { return a < b ? a : b; }
     static constexpr T id() { return max_value; }
 };
 
 template<class T, T min_value = infinity<T>::min> struct Max {
     using value_type = T;
-    static constexpr T op(const T& a, const T& b) { return a < b ? b : a;}
+    static constexpr T op(const T& a, const T& b) { return a < b ? b : a; }
     static constexpr T id() { return min_value; }
 };
 
@@ -34,16 +34,12 @@ template<class T, T max_value = infinity<T>::max> struct AssignMin {
     using M = Min<T, max_value>;
     using E = Assign<T>;
     static constexpr T op(const T& a, const T&) { return a; }
-    static constexpr T mul(const T& a, int) { return a; }
-    static constexpr T mul_op(const T& a, int, const T&) { return a; }
 };
 
 template<class T, T min_value = infinity<T>::min> struct AssignMax {
     using M = Max<T, min_value>;
     using E = Assign<T>;
     static constexpr T op(const T& a, const T&) { return a; }
-    static constexpr T mul(const T& a, int) { return a; }
-    static constexpr T mul_op(const T& a, int, const T&) { return a; }
 };
 
 template<class T> struct AssignSum {
@@ -58,16 +54,12 @@ template<class T, T max_value = infinity<T>::max> struct AddMin {
     using M = Min<T, max_value>;
     using E = Sum<T>;
     static constexpr T op(const T& a, const T& b) { return b + a; }
-    static constexpr T mul(const T& a, int) { return a; }
-    static constexpr T mul_op(const T& a, int, const T& c) { return c + a; }
 };
 
 template<class T, T min_value = infinity<T>::min> struct AddMax {
     using M = Max<T, min_value>;
     using E = Sum<T>;
     static constexpr T op(const T& a, const T& b) { return b + a; }
-    static constexpr T mul(const T& a, int) { return a; }
-    static constexpr T mul_op(const T& a, int, const T& c) { return c + a; }
 };
 
 template<class T> struct AddSum {
@@ -75,39 +67,33 @@ template<class T> struct AddSum {
     using E = Sum<T>;
     static constexpr T op(const T& a, const T& b) { return b + a; }
     static constexpr T mul(const T& a, int b) { return a * b; }
-    static constexpr T mul_op(const T& a, int b, const T& c) { return c + a * b; }
+    static constexpr T mul_op(const T& a, int b, const T& c) {
+        return c + a * b;
+    }
 };
 
 template<class T, T max_value = infinity<T>::max> struct ChminMin {
     using M = Min<T, max_value>;
     using E = Min<T>;
     static constexpr T op(const T& a, const T& b) { return std::min(b, a); }
-    static constexpr T mul(const T& a, int) { return a; }
-    static constexpr T mul_op(const T& a, int, const T& c) { return std::min(c, a); }
 };
 
 template<class T, T min_value = infinity<T>::min> struct ChminMax {
     using M = Max<T, min_value>;
     using E = Min<T>;
     static constexpr T op(const T& a, const T& b) { return std::min(b, a); }
-    static constexpr T mul(const T& a, int) { return a; }
-    static constexpr T mul_op(const T& a, int, const T& c) { return std::min(c, a); }
 };
 
 template<class T, T max_value = infinity<T>::max> struct ChmaxMin {
     using M = Min<T, max_value>;
     using E = Max<T>;
     static constexpr T op(const T& a, const T& b) { return std::max(b, a); }
-    static constexpr T mul(const T& a, int) { return a; }
-    static constexpr T mul_op(const T& a, int, const T& c) { return std::max(c, a); }
 };
 
 template<class T, T min_value = infinity<T>::min> struct ChmaxMax {
     using M = Max<T, min_value>;
     using E = Max<T>;
     static constexpr T op(const T& a, const T& b) { return std::max(b, a); }
-    static constexpr T mul(const T& a, int) { return a; }
-    static constexpr T mul_op(const T& a, int, const T& c) { return std::max(c, a); }
 };
 
 
@@ -116,8 +102,14 @@ template<class M> struct ReverseMonoid {
     static value_type op(const value_type& a, const value_type& b) {
         return M::op(b, a);
     }
-    static value_type id() { return M::id(); }
-    static value_type get_inv(const value_type& a) { return M::get_inv(a); }
+    static value_type id() {
+        static_assert(has_id<M>::value, "id is not defined");
+        return M::id();
+    }
+    static value_type get_inv(const value_type& a) {
+        static_assert(has_get_inv<M>::value, "get_inv is not defined");
+        return M::get_inv(a);
+    }
 };
 
 template<class M_> struct AttachEffector {
@@ -136,36 +128,52 @@ template<class E_> struct AttachMonoid {
 
 
 template<class M, class = void> class has_op : public std::false_type {};
-template<class M> class has_op<M, decltype((void)M::op)> : public std::true_type {};
+template<class M>
+class has_op<M, decltype((void)M::op)> : public std::true_type {};
 
 template<class M, class = void> class has_id : public std::false_type {};
-template<class M> class has_id<M, decltype((void)M::id)> : public std::true_type {};
+template<class M>
+class has_id<M, decltype((void)M::id)> : public std::true_type {};
 
 template<class M, class = void> class has_inv : public std::false_type {};
-template<class M> class has_inv<M, decltype((void)M::inv)> : public std::true_type {};
+template<class M>
+class has_inv<M, decltype((void)M::inv)> : public std::true_type {};
 
 template<class M, class = void> class has_get_inv : public std::false_type {};
-template<class M> class has_get_inv<M, decltype((void)M::get_inv)> : public std::true_type {};
-
+template<class M>
+class has_get_inv<M, decltype((void)M::get_inv)> : public std::true_type {};
 
 template<class A, class = void> class has_mul : public std::false_type {};
-template<class A> class has_mul<A, decltype((void)A::mul)> : public std::true_type {};
+template<class A>
+class has_mul<A, decltype((void)A::mul)> : public std::true_type {};
 
 template<class A, class = void> class has_mul_op : public std::false_type {};
-template<class A> class has_mul_op<A, decltype((void)A::mul_op)> : public std::true_type {};
-
+template<class A>
+class has_mul_op<A, decltype((void)A::mul_op)> : public std::true_type {};
 
 template<class T, class = void> class is_semigroup : public std::false_type {};
-template<class T> class is_semigroup<T, decltype(std::declval<typename T::value_type>(), (void)T::op)> : public std::true_type {};
+template<class T>
+class is_semigroup<T, decltype(std::declval<typename T::value_type>(),
+                               (void)T::op)> : public std::true_type {};
 
-template<class T, class = void> class is_monoid : public std::false_type {};;
-template<class T> class is_monoid<T, decltype(std::declval<typename T::value_type>(), (void)T::op, (void)T::id)> : public std::true_type {};
+template<class T, class = void> class is_monoid : public std::false_type {};
 
-template<class T, class = void> class is_group : public std::false_type {};;
-template<class T> class is_group<T, decltype(std::declval<typename T::value_type>(), (void)T::op, (void)T::id, (void)T::get_inv)> : public std::true_type {};
+template<class T>
+class is_monoid<T, decltype(std::declval<typename T::value_type>(), (void)T::op,
+                            (void)T::id)> : public std::true_type {};
+
+template<class T, class = void> class is_group : public std::false_type {};
+
+template<class T>
+class is_group<T, decltype(std::declval<typename T::value_type>(), (void)T::op,
+                           (void)T::id, (void)T::get_inv)>
+    : public std::true_type {};
 
 template<class T, class = void> class is_action : public std::false_type {};
-template<class T> class is_action<T, typename std::enable_if<
-        is_semigroup<typename T::M>::value && is_semigroup<typename T::E>::value && has_op<T>::value>::type> : public std::true_type {};
+template<class T>
+class is_action<T, typename std::enable_if<is_monoid<typename T::M>::value &&
+                                           is_semigroup<typename T::E>::value &&
+                                           has_op<T>::value>::type>
+    : public std::true_type {};
 
 } // namespace Monoid
