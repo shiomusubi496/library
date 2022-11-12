63faeefb62bd6d7864c68ef7d6c779933ff10113
shiomusubi496 <katsuki.ohta@gmail.com>
2022-11-12 14:37:48 +0900

    Fix CRT

---
+++ test/yuki/447-CRT.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://yukicoder.me/problems/447"
+#include "../../other/template.hpp"
+#include "../../math/ChineseRemainder.hpp"
+using namespace std;
+int main() {
+    vector<ll> A(3), B(3);
+    rep (i, 3) scan >> A[i] >> B[i];
+    auto p = ChineseRemainder(A, B);
+    prints(p.first);
+}
