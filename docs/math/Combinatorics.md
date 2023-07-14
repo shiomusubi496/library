597cdbd234a9dd31692b4047135afb16baca1f89
shiomusubi496 <katsuki.ohta@gmail.com>
2023-07-14 20:30:02 +0900

    Fix Combinatorics

--- docs/math/Combinatorics.md
+++ docs/math/Combinatorics.md
@@ -2,13 +2,14 @@
 
 ${}_nC_r$ などを計算する。
 
-- `IntCombinatorics<T>` : mod を取らない場合に使う。
 - `Combinatorics<T>` : mod を取る場合に使う。 `T` は modint にする。
 
 以下の計算ができる。
 
 - `void init(ll n)` : `n` 以下の値を計算できるようにする。全体で $\Theta(\max(n) + \log mod)$ 。
 - `T fact(ll x)` : $x!$ を計算する。 $\Theta(1)$ 。
+- `T finv(ll x)` : $1/x!$ を計算する。 $\Theta(1)$ 。
+- `T inv(ll x)` : $1/x$ を計算する。 $\Theta(1)$ 。
 - `T perm(ll n, ll r)` : ${}_nP_r$ を計算する。 $\Theta(1)$ 。
 - `T comb(ll n, ll r)` : ${}_nC_r$ を計算する。 $\Theta(1)$ 。
 - `T homo(ll n, ll r)` : ${}_nH_r$ を計算する。 $\Theta(1)$ 。
--- math/Combinatorics.hpp
+++ math/Combinatorics.hpp
@@ -3,54 +3,6 @@
 #include "../other/template.hpp"
 #include "ModInt.hpp"
 
-template<class T> class IntCombinatorics {
-private:
-    static std::vector<T> factorial;
-
-public:
-    static void init(ll n) {
-        const int b = factorial.size();
-        if (n < b) return;
-        factorial.resize(n + 1);
-        rep (i, b, n + 1) factorial[i] = factorial[i - 1] * i;
-    }
-    static T fact(ll x) {
-        init(x);
-        return factorial[x];
-    }
-    static T perm(ll n, ll r) {
-        if (r < 0 || r > n) return T(0);
-        init(n);
-        return factorial[n] / factorial[n - r];
-    }
-    static T comb(ll n, ll r) {
-        if (n < 0) return T(0);
-        if (r < 0 || r > n) return T(0);
-        init(n);
-        return factorial[n] / factorial[n - r] / factorial[r];
-    }
-    static T homo(ll n, ll r) { return comb(n + r - 1, r); }
-    static T small_perm(ll n, ll r) {
-        if (r < 0 || r > n) return 0;
-        chmin(r, n - r);
-        T res = 1;
-        reps (i, r) res *= n - r + i;
-        return res;
-    }
-    static T small_comb(ll n, ll r) {
-        if (r < 0 || r > n) return 0;
-        chmin(r, n - r);
-        init(r);
-        T res = 1;
-        reps (i, r) res *= n - r + i;
-        return res / factorial[r];
-    }
-    static T small_homo(ll n, ll r) { return small_comb(n + r - 1, r); }
-};
-
-template<class T>
-std::vector<T> IntCombinatorics<T>::factorial = std::vector<T>(1, 1);
-
 template<class T> class Combinatorics {
 private:
     static std::vector<T> factorial;
@@ -73,6 +25,10 @@ public:
     static T finv(ll x) {
         init(x);
         return factinv[x];
+    }
+    static T inv(ll x) {
+        init(x);
+        return factorial[x - 1] * factinv[x];
     }
     static T perm(ll n, ll r) {
         if (r < 0 || r > n) return 0;
