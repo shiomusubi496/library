e9e4f8e32d36ad010f88f381077a4f2b59374c35
shiomusubi496 <katsuki.ohta@gmail.com>
2024-04-18 14:58:23 +0900

    Format

--- math/convolution/MinPlusConvolution.hpp
+++ math/convolution/MinPlusConvolution.hpp
@@ -4,13 +4,15 @@
 #include "../../dp/SMAWK.hpp"
 
 template<class T>
-std::vector<T> min_plus_convolution(const std::vector<T> &a, const std::vector<T> &b) {
+std::vector<T> min_plus_convolution(const std::vector<T>& a,
+                                    const std::vector<T>& b) {
     int n = a.size(), m = b.size();
     // b must be convex
     rep (i, m - 2) assert(b[i + 1] - b[i] <= b[i + 2] - b[i + 1]);
     auto idx = smawk_comp(n + m - 1, n, [&](int i, int j, int k) {
         if (i - j < 0 || i - j >= m || i - k < 0 || i - k >= m) {
-            return std::max(j - i - 1, i - j - m) < std::max(k - i - 1, i - k - m);
+            return std::max(j - i - 1, i - j - m) <
+                   std::max(k - i - 1, i - k - m);
         }
         return a[j] + b[i - j] < a[k] + b[i - k];
     });
