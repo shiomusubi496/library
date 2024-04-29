aef05e825408272e104ade58a4e3f275df6e0065
shiomusubi496 <katsuki.ohta@gmail.com>
2024-04-29 23:15:20 +0900

    Create BEST

--- graph/mst/CountSpanningTree.hpp
+++ graph/mst/CountSpanningTree.hpp
@@ -4,14 +4,22 @@
 #include "../../math/matrix/Matrix.hpp"
 #include "../../math/matrix/Determinant.hpp"
 
-template<class T, class U> T count_spanning_tree(const Graph<U>& G) {
+template<class T, class U> T count_spanning_tree(const Graph<U>& G, int v = 0) {
     const int N = G.size();
     Matrix<T> A(N - 1, N - 1);
-    rep (i, N - 1) A[i][i] += G[i + 1].size();
-    rep (i, 1, N) {
-        each_const (e : G[i]) {
-            if (e.to == 0) continue;
-            --A[i - 1][e.to - 1];
+    rep (i, N) {
+        if (i == v) continue;
+        T cnt = 0;
+        for (const auto& e : G[i]) {
+            if (e.to != i) cnt += e.cost;
+        }
+        A[i < v ? i : i - 1][i < v ? i : i - 1] = cnt;
+    }
+    rep (i, N) {
+        if (i == v) continue;
+        for (const auto& e : G[i]) {
+            if (e.to == v || e.to == i) continue;
+            A[i < v ? i : i - 1][e.to < v ? e.to : e.to - 1] -= e.cost;
         }
     }
     return determinant(A);
