c535adc8be592f973f120e05d69d6d733e1d0d09
shiomusubi496 <katsuki.ohta@gmail.com>
2024-05-31 09:59:04 +0900

    Create WildPatternMatching

--- string/WildcardPatternMatching.hpp
+++ string/WildcardPatternMatching.hpp
@@ -9,26 +9,94 @@ namespace internal {
 
 template<class T>
 std::vector<bool> wildcard_mod(const std::vector<int>& a,
-                               const std::vector<int>& b, bool a0, bool b0) {
+                               const std::vector<int>& b) {
     const int n = a.size(), m = b.size();
     const int lg = bitop::ceil_log2(n), N = 1 << lg;
     std::vector<T> A1(N), B1(N), A2(N), B2(N), A3(N), B3(N);
-    rep (i, n) A1[i] = a0 ? a[i] : 1;
-    rep (i, m) B1[i] = b0 ? b[m - 1 - i] : 1;
-    rep (i, n) A2[i] = A1[i] * A1[i];
-    rep (i, m) B2[i] = B1[i] * B1[i];
-    rep (i, n) A3[i] = A1[i] * A2[i];
-    rep (i, m) B3[i] = B1[i] * B2[i];
-    number_theoretic_transform(A1);
-    number_theoretic_transform(B1);
+    std::vector<T> C1(N), C2(n - m + 1);
+    rep (i, n) A1[i] = a[i] == 0 ? 0 : 1;
+    rep (i, m) B1[i] = b[m - 1 - i] == 0 ? 0 : 1;
+    rep (i, n) A2[i] = a[i] * A1[i];
+    rep (i, m) B2[i] = b[m - 1 - i] * B1[i];
+    rep (i, n) A3[i] = a[i] * A2[i];
+    rep (i, m) B3[i] = b[m - 1 - i] * B2[i];
+    if (find(all(a), 0) == a.end()) {
+        T sm = 0;
+        rep (i, m) sm += B3[i];
+        rep (i, n - m + 1) C2[i] += sm;
+    }
+    else {
+        number_theoretic_transform(A1);
+        number_theoretic_transform(B3);
+        rep (i, N) C1[i] += A1[i] * B3[i];
+    }
+    if (find(all(b), 0) == b.end()) {
+        std::vector<T> cum(n + 1);
+        rep (i, n) cum[i + 1] = cum[i] + A3[i];
+        rep (i, n - m + 1) C2[i] += cum[i + m] - cum[i];
+    }
+    else {
+        number_theoretic_transform(A3);
+        number_theoretic_transform(B1);
+        rep (i, N) C1[i] += A3[i] * B1[i];
+    }
     number_theoretic_transform(A2);
     number_theoretic_transform(B2);
-    number_theoretic_transform(A3);
-    number_theoretic_transform(B3);
-    rep (i, N) A1[i] = A1[i] * B3[i] - 2 * A2[i] * B2[i] + A3[i] * B1[i];
-    inverse_number_theoretic_transform(A1);
+    rep (i, N) C1[i] -= 2 * A2[i] * B2[i];
+    inverse_number_theoretic_transform(C1);
     std::vector<bool> c(n - m + 1);
-    rep (i, n - m + 1) c[i] = A1[i + m - 1] == 0;
+    rep (i, n - m + 1) c[i] = (C1[i + m - 1] + C2[i] == 0);
+    return c;
+}
+
+template<class T>
+std::vector<bool> wildcard_random(const std::vector<int>& a,
+                                  const std::vector<int>& b) {
+    const int n = a.size(), m = b.size();
+    const int lg = bitop::ceil_log2(n), N = 1 << lg;
+    bool a0 = find(all(a), 0) == a.end();
+    bool b0 = find(all(b), 0) == b.end();
+    std::vector<T> A1(N), B1(N), A2(N), B2(N);
+    std::vector<T> C1(N), C2(n - m + 1);
+    if (a0) {
+        rep (i, n) A1[i] = a[i] == 0 ? 0 : 1;
+        rep (i, m) {
+            B1[i] = b[m - 1 - i] == 0
+                        ? 0
+                        : rand32.uniform<int>(1, T::get_mod() - 1);
+        }
+    }
+    else {
+        rep (i, n) {
+            A1[i] = a[i] == 0 ? 0 : rand32.uniform<int>(1, T::get_mod() - 1);
+        }
+        rep (i, m) B1[i] = b[m - 1 - i] == 0 ? 0 : 1;
+    }
+    rep (i, n) A2[i] = a[i] * A1[i];
+    rep (i, m) B2[i] = b[m - 1 - i] * B1[i];
+    if (a0) {
+        T sm = 0;
+        rep (i, m) sm += B2[i];
+        rep (i, n - m + 1) C2[i] -= sm;
+    }
+    else {
+        number_theoretic_transform(A1);
+        number_theoretic_transform(B2);
+        rep (i, N) C1[i] -= A1[i] * B2[i];
+    }
+    if (!a0 && b0) {
+        std::vector<T> cum(n + 1);
+        rep (i, n) cum[i + 1] = cum[i] + A2[i];
+        rep (i, n - m + 1) C2[i] += cum[i + m] - cum[i];
+    }
+    else {
+        number_theoretic_transform(A2);
+        number_theoretic_transform(B1);
+        rep (i, N) C1[i] += A2[i] * B1[i];
+    }
+    inverse_number_theoretic_transform(C1);
+    std::vector<bool> c(n - m + 1);
+    rep (i, n - m + 1) c[i] = (C1[i + m - 1] + C2[i] == 0);
     return c;
 }
 
@@ -39,31 +107,19 @@ std::vector<bool> wildcard_pattern_matching(std::vector<int> a,
                                             bool deterministic = true) {
     const int n = a.size(), m = b.size();
     if (n < m) return {};
-    bool a0 = find(all(a), 0) != a.end(), b0 = find(all(b), 0) != b.end();
     i128 MAX_VAL = (i128)MAX * MAX * m;
-    if (a0) MAX_VAL *= MAX;
-    if (b0) MAX_VAL *= MAX;
     static constexpr int MOD1 = 2113929217, MOD2 = 2013265921,
-                         MOD3 = 1811939329, MOD4 = 1711276033;
+                         MOD3 = 1811939329;
     if (!deterministic) {
-        MAX_VAL = 1;
-        std::vector<int> val(MAX + 1);
-        rep (i, 1, MAX + 1) val[i] = rand32() % (MOD1 - 1) + 1;
-        rep (i, n) a[i] = val[a[i]];
-        rep (i, m) b[i] = val[b[i]];
+        return internal::wildcard_random<static_modint<MOD1>>(a, b);
     }
-    std::vector<bool> res =
-        internal::wildcard_mod<static_modint<MOD1>>(a, b, a0, b0);
+    std::vector<bool> res = internal::wildcard_mod<static_modint<MOD1>>(a, b);
     if (MAX_VAL >= MOD1) {
-        auto c = internal::wildcard_mod<static_modint<MOD2>>(a, b, a0, b0);
+        auto c = internal::wildcard_mod<static_modint<MOD2>>(a, b);
         rep (i, n - m + 1) res[i] = res[i] && c[i];
     }
     if (MAX_VAL >= (i128)MOD1 * MOD2) {
-        auto c = internal::wildcard_mod<static_modint<MOD3>>(a, b, a0, b0);
-        rep (i, n - m + 1) res[i] = res[i] && c[i];
-    }
-    if (MAX_VAL >= (i128)MOD1 * MOD2 * MOD3) {
-        auto c = internal::wildcard_mod<static_modint<MOD4>>(a, b, a0, b0);
+        auto c = internal::wildcard_mod<static_modint<MOD3>>(a, b);
         rep (i, n - m + 1) res[i] = res[i] && c[i];
     }
     return res;
--- test/yosupo/new/wildcard_pattern_matching.test.cpp
+++ test/yosupo/new/wildcard_pattern_matching.test.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 int main() {
     string S, T; scan >> S >> T;
-    auto res = wildcard_pattern_matching(S, T);
+    auto res = wildcard_pattern_matching(S, T, '*', false);
+    auto res2 = wildcard_pattern_matching(S, T, '*', true);
+    assert(res == res2);
     rep (i, res.size()) print << res[i];
     print << endl;
 }
