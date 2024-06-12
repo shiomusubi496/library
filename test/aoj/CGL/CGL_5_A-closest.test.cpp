1b9ddf61eb446cbb7475a74c78b1d5564b42fa0b
shiomusubi496 <katsuki.ohta@gmail.com>
2024-06-12 23:55:19 +0900

    Fix

--- test/aoj/CGL/CGL_5_A-closest.test.cpp
+++ test/aoj/CGL/CGL_5_A-closest.test.cpp
@@ -1,5 +1,6 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/CGL_5_A"
 #define ERROR 0.000001
+#define GEOMETRY_EPS 1e-12
 #include "../../../other/template.hpp"
 #include "../../../geometry/Polygon.hpp"
 using namespace std;
