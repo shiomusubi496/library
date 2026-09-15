c7e5705ee6606d145f8f61755c9b0140dc401702
shiomusubi496 <katsuki@sphene.localdomain>
2026-09-16 00:51:29 +0900

    Fix test

--- test/yuki/3030-MRPrime.test.cpp
+++ test/yuki/3030-MRPrime.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://yukicoder.me/problems/no/3030"
+#define PROBLEM "https://yukicoder.me/problems/no/8030"
 #include "../../other/template.hpp"
 #include "../../math/num/MillerRabin.hpp"
 using namespace std;
